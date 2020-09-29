3ab137dd3e70d4d693fd5394d443586cf19a3429
danworth <dan.worth@digital.cabinet-office.gov.uk>
2020-09-29 15:23:12 +0100

    init empty grid

--- game.js
+++ game.js
@@ -1,4 +1,18 @@
 class Game {
+
+    constructor(size) {
+      this.grid = this.initGrid(size)
+    }
+
+    initGrid(size) {
+      let grid = new Array(size)
+      for(let row = 0; row < size; row++){
+        let newCol = new Array(size)
+        grid[row] = newCol
+      }
+      return grid
+    }
+
     getLiveNumberOfCells() {
         return 0;
     }
--- game.test.js
+++ game.test.js
@@ -11,3 +11,9 @@ test('when a game has no live cells the next round also has no live cells', () =
   game = game.nextRond();
   expect(game.getLiveNumberOfCells()).toBe(0)
 })
+
+test('should create an initial empty grid of the correct size', () => {
+  const game = new Game(5);
+  expect(game.grid.length).toBe(5)
+  expect(game.grid[0].length).toBe(5)
+})
