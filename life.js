bff84f87344431702bc504d1ca497f00dae6a6d9
danworth <dan.worth@digital.cabinet-office.gov.uk>
2020-09-29 14:45:41 +0100

    WIP

--- life.js
+++ life.js
@@ -1,23 +1,26 @@
 // return 2D array
 // each cell can be alive or dead (true or false)
 // true is alive, false is dead
-function generateBaseGrid(size, coords) {
-    let grid = []
-    for (let rowIndex = 0; rowIndex < size; rowIndex++) {
-        let column = []
-        // sfount JavaScript hacks
-        // grid.push(new Array(size).map(() => false))
-        for (let columnIndex = 0; columnIndex < size; columnIndex++) {
-            column.push(false)
-        }
-        grid.push(column)
-    }
+class Grid {
 
-    coords.forEach(element => {
-        grid[element.x][element.y] = true
-    });
+  generateBaseGrid(size, coords) {
+      let grid = []
+      for (let rowIndex = 0; rowIndex < size; rowIndex++) {
+          let column = []
+          // sfount JavaScript hacks
+          // grid.push(new Array(size).map(() => false))
+          for (let columnIndex = 0; columnIndex < size; columnIndex++) {
+              column.push(false)
+          }
+          grid.push(column)
+      }
 
-    return grid
+      coords.forEach(element => {
+          grid[element.x][element.y] = true
+      });
+
+      return grid
+  }
+
 }
-
-module.exports.generateBaseGrid = generateBaseGrid
+module.exports.Grid = Grid
--- life.test.js
+++ life.test.js
@@ -1,21 +1,34 @@
-const { generateBaseGrid } = require('./life')
+const { Grid } = require('./life')
 
 test('returns a square two dimensional array', () => {
-    const grid = generateBaseGrid(5, [])
+    const grid = new Grid()
+    const myGrid = grid.generateBaseGrid(5, [])
 
-    expect(grid.length).toBe(5)
-    expect(grid[0].length).toBe(5)
+    expect(myGrid.length).toBe(5)
+    expect(myGrid[0].length).toBe(5)
 })
 
 test('correctly sets starting live cells', () => {
-    const grid = generateBaseGrid(5, [
+    const grid = new Grid()
+    const myGrid = grid.generateBaseGrid(5, [
         {x: 1, y: 1},
         {x: 2, y: 2},
-        {x: 3, y: 3} 
+        {x: 3, y: 3}
     ])
 
-    expect(grid[0][0]).toBe(false)
-    expect(grid[1][1]).toBe(true)
-    expect(grid[2][2]).toBe(true)
-    expect(grid[3][3]).toBe(true)
+    expect(myGrid[0][0]).toBe(false)
+    expect(myGrid[1][1]).toBe(true)
+    expect(myGrid[2][2]).toBe(true)
+    expect(myGrid[3][3]).toBe(true)
 })
+
+test('should count a cells live neighbours', () => {
+    const grid = new Grid()
+    const myGrid = grid.generateBaseGrid(5, [
+        {x: 1, y: 1},
+        {x: 2, y: 2},
+        {x: 3, y: 3}
+    ])
+})
+
+
